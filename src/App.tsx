ca4f39f318e19770eb5adc7e9a1f177f9236ec88
David Mugisha <davidkhalifa47@gmail.com>
2025-04-11 20:06:23 +0200

    404

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useState } from "react";
 import Index from "./pages/Index";
-// import NotFound from "./pages/NotFound";
+
 function App() {
   const [queryClient] = useState(() => new QueryClient());
   return (
@@ -10,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
-          {/* <Route path="*" element={<NotFound />} /> */}
+          <Route path="*" element={<h1>404 NOT FOUND!</h1>} />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
